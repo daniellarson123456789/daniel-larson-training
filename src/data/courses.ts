fecab9331f87b568c15e8462f665e72f630c9ba3
daniellarson123456789 <daniel@gorealestatelicense.com>
2026-09-20 07:46:15 +0000

    Final portal and FastPass logo fixes

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -5,7 +5,7 @@ export type CourseDirectoryItem = {
 };
 
 export const salesLicensingCourses: CourseDirectoryItem[] = [
-  { label: "Start Here", href: "https://portal.daniellarsontraining.com/courses/starthere", note: "Roadmap" },
+  { label: "FastPass Start Here", href: "https://portal.daniellarsontraining.com/courses/starthere", note: "Roadmap" },
   { label: "Course 1", href: "https://portal.daniellarsontraining.com/courses/course1", note: "Required pre-license course" },
   { label: "Course 1 Exam Pass", href: "https://portal.daniellarsontraining.com/courses/examprep", note: "Standard course-final / review path" },
   { label: "FastPass", href: "https://portal.daniellarsontraining.com/courses/FASTPASS", note: "FastPass certification path" },
